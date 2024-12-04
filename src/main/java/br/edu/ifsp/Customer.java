bc16d0c08c69899865be3370bd4d9e6b1d400efa
heitorlemescaldas <heitor.caldas@aluno.ifsp.edu.br>
2024-12-04 12:27:31 -0300

    delega cálculo de pontos de locação para a hierarquia price

--- src/main/java/br/edu/ifsp/Customer.java
+++ src/main/java/br/edu/ifsp/Customer.java
@@ -17,25 +17,21 @@ class Customer extends DomainObject {
         String result = "Rental Record for " + getName() + "\n";
 
         for (Rental each : rentals) {
-            // O cálculo do preço é delegado diretamente à Tool
             double thisAmount = each.getToolItem().getTool().calculateAmount(each.getDaysRented());
-
-            // Soma o total geral
             totalAmount += thisAmount;
 
-            // O cálculo de pontos de locação frequente é delegado à Tool
+            // Delegação do cálculo de pontos
             frequentRenterPoints += each.getToolItem().getTool().calculateFrequentRenterPoints(each.getDaysRented());
 
-            // Adiciona detalhes ao relatório
             result += "\t" + each.getToolItem().getTool().getName() + "\t" + thisAmount + "\n";
         }
 
-        // Adiciona o rodapé ao relatório
         result += "Amount owed is " + totalAmount + "\n";
         result += "You earned " + frequentRenterPoints + " frequent renter points";
 
         return result;
     }
+
     public void addRental(Rental arg) {
         rentals.add(arg);
     }
